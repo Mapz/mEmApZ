07662e2209f0fddf06ea49f7fe886bf178747480
Mapz <61966578@qq.com>
2016-08-02 12:31:49 +0800

    第一次提交，创建django项目

---
+++ mExApZ/mExApZ/urls.py
@@ -0,0 +1,25 @@
+"""mExApZ URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+#设置管理界面title和header#
+admin.site.site_header = 'mExApZ管理'
+admin.site.site_title = 'mExApZ管理'
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
---
+++ mExApZ/startServer.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+python3 manage.py runserver 192.168.59.171:8000
