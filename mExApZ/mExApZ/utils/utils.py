3825abcd2cdadbad7b50e9ac605f3e7960c3dff1
Mapzchen <mapzchen@Urabe-Macbookpro.local>
2016-09-10 16:45:27 +0800

    增加ignore，修改错误依赖

--- mExApZ/mExApZ/utils/utils.py
+++ mExApZ/mExApZ/utils/utils.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 #-*- encoding:utf-8 -*-
 import hashlib
-from wechat_sdk import WechatBasic
 
 token = "huihuiAiyaya"
 encrypt_key = "hW1KgvdaAYyP3ATPM1TQU9kzXtVeApya8AnXanS5TEe"
